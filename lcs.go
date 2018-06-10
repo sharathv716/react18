6e78c12efe95080d5001e0c2207cf87819d52970
sharathv716 <sharathv716@gmail.com>
2018-06-10 10:07:52 -0700

    First Versions of Recursion Programs

---
+++ lcs.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "fmt"
+)
+var i, j, count int
+var mystring []string
+
+func max(s1, s2 int) int{
+  if s1 < s2{
+    return s2
+  }
+  return s1
+}
+
+func lcs(str1, str2 string) int{
+  count++
+  if i > len(str1) - 1  || j > len(str2) - 1{
+    return 0
+  }
+  if str1[i] == str2[j]{
+    return 1 + lcs(str1[i+1:], str2[j+1:])
+  }else{
+    return max(lcs(str1[i+1:], str2[j:]), lcs(str1[i:], str2[j+1:]))
+  }
+}
+
+func main(){
+  fmt.Println(lcs("AGGTAB","GXTXAYB"))
+  fmt.Println("Number of calls ", count)
+  fmt.Println(mystring)
+}
